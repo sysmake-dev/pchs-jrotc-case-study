e9590c33d9e1e3ee7f0e1273e2441021e82b6226
sysmake-dev <github@sysmake.net>
2026-07-06 05:57:59 +0000

    Create events.js

---
+++ src/assets/js/events.js
@@ -0,0 +1,35 @@
+async function loadEvents() {
+  const response = await fetch("../../content/events.json");
+  const events = await response.json();
+
+  const eventList = document.getElementById("event-list");
+
+  if (!eventList) return;
+
+  const page = document.querySelector("main").dataset.page;
+
+  let eventsToRender = events;
+
+  if (page === "home") {
+    eventsToRender = events.slice(0, 3);
+  }
+
+  renderEvents(eventList, eventsToRender);
+}
+
+function renderEvents(container, events) {
+  container.innerHTML = "";
+
+  for (const event of events) {
+    container.innerHTML += `
+      <article class="event item">
+        <h3>${event.title}</h3>
+        <p>${event.date}</p>
+        <p>${event.location}</p>
+        <p>${event.summary}</p>
+      </article>
+    `;
+  }
+}
+
+loadEvents();
