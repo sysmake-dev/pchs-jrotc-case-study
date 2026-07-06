1171c6be796960889ca9ec6c2df774512e07d71a
sysmake-dev <github@sysmake.net>
2026-07-06 05:57:41 +0000

    Create announcements.js

---
+++ src/assets/js/announcements.js
@@ -0,0 +1,36 @@
+async function loadAnnouncements() {
+  const response = await fetch("../../content/announcements.json");
+  const announcements = await response.json();
+
+  const announcementList = document.getElementById("announcement-list");
+
+  if (!announcementList) return;
+
+  const page = document.querySelector("main").dataset.page;
+
+  let announcementsToRender = announcements;
+
+  if (page === "home") {
+    announcementsToRender = announcements.filter(
+      (announcement) => announcement.current,
+    );
+  }
+
+  renderAnnouncements(announcementList, announcementsToRender);
+}
+
+function renderAnnouncements(container, announcements) {
+  container.innerHTML = "";
+
+  for (const announcement of announcements) {
+    container.innerHTML += `
+      <article id="item-id" class="announcement item dark">
+        <h3>${announcement.title}</h3>
+        <p>${announcement.date}</p>
+        <p>${announcement.summary}</p>
+      </article>
+    `;
+  }
+}
+
+loadAnnouncements();
